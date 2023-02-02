52b25e03cf1d5674062e6e86033288af4716b12a
aliah1105 <aliahadi.f1105@gmail.com>
2023-02-02 09:52:34 +0330

    first commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import { Box, createTheme, Stack, ThemeProvider } from "@mui/material";
+import { useState } from "react";
+import Add from "./components/Add";
+import Feed from "./components/Feed";
+import Navbar from "./components/Navbar";
+import RightBar from "./components/RightBar";
+import Sidebar from "./components/Sidebar";
+
+function App() {
+  const [mode, setMode] = useState("dark");
+
+  const darkTheme = createTheme({
+    palette: {
+      mode: mode,
+    },
+  });
+
+  return (
+    <div>
+      <ThemeProvider theme={darkTheme} >
+        <Box bgcolor={"background.default"} color={"text.primary"}  >
+          <Navbar />
+          <Stack direction="row" spacing={2} justifyContent="space-between">
+            <Sidebar setMode={setMode} mode={mode} />
+            <Feed />
+            <RightBar />
+          </Stack>
+          <Add />
+        </Box>
+      </ThemeProvider>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,105 @@
+import { Mail, Notifications, Pets } from "@mui/icons-material";
+import {
+  AppBar,
+  Avatar,
+  Badge,
+  Box,
+  InputBase,
+  Menu,
+  MenuItem,
+  styled,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import React, { useState } from "react";
+
+const Navbar = () => {
+  const [open, setOpen] = useState(false);
+
+  const StyledToolbar = styled(Toolbar)({
+    display: "flex",
+    justifyContent: "space-between",
+  });
+
+  const Search = styled("div")(({ theme }) => ({
+    backgroundColor: "white",
+    padding: "0 10px",
+    borderRadius: theme.shape.borderRadius,
+    width: "40%",
+  }));
+
+  const Icons = styled(Box)(({ theme }) => ({
+    display: "none",
+    gap: "20px",
+    alignItems: "center",
+    [theme.breakpoints.up("sm")]: {
+      display: "flex",
+    },
+  }));
+
+  const UserBox = styled(Box)(({ theme }) => ({
+    display: "flex",
+    alignItems: "center",
+    gap: "10px",
+    [theme.breakpoints.up("sm")]: {
+      display: "none",
+    },
+  }));
+
+  return (
+    <AppBar position="sticky">
+      <StyledToolbar>
+        <Typography variant="h6" sx={{ display: { xs: "none", sm: "block" } }}>
+          Snoopy media
+        </Typography>
+        <Pets sx={{ display: { xs: "block", sm: "none" } }} />
+        <Search>
+          <InputBase placeholder="Search..." />
+        </Search>
+        <Icons>
+          <Badge color="error" badgeContent={4}>
+            <Mail />
+          </Badge>
+          <Badge color="error" badgeContent={2}>
+            <Notifications />
+          </Badge>
+          <Avatar
+            onClick={(e) => setOpen(true)}
+            sx={{ width: 30, height: 30, "&:hover": { cursor:"pointer" } }}
+            alt="Remy Sharp"
+            src="https://images.pexels.com/photos/846741/pexels-photo-846741.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          />
+        </Icons>
+        <UserBox>
+          <Avatar
+            onClick={(e) => setOpen(true)}
+            sx={{ width: 30, height: 30, "&:hover": { cursor:"pointer" } }}
+            alt="Remy Sharp"
+            src="https://images.pexels.com/photos/846741/pexels-photo-846741.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          />
+          <Typography variant="span">Ali</Typography>
+        </UserBox>
+      </StyledToolbar>
+      <Menu
+        onClose={(e) => setOpen(false)}
+        id="demo-positioned-menu"
+        aria-labelledby="demo-positioned-button"
+        open={open}
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+      >
+        <MenuItem>Profile</MenuItem>
+        <MenuItem>My account</MenuItem>
+        <MenuItem>Logout</MenuItem>
+      </Menu>
+    </AppBar>
+  );
+};
+
+export default Navbar;
